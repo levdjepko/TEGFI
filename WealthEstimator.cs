86d06f7492663e699f282ddab540d32564abfc16
Lev Dzhepko <levdjepko@gmail.com>
2020-04-21 15:21:57 -0700

    Changed tests

--- WealthEstimator.cs
+++ WealthEstimator.cs
@@ -4,14 +4,18 @@
     {
         public static double EstimateTenYearsAmount_Monthly(double inputAmount)
         {
-            double totalAmount = 0.0;
-
-            for (int i = 0; i < 120; i++)
+            if (inputAmount > 0.0)
             {
-                totalAmount = totalAmount + totalAmount * 0.07 / 12.0 + inputAmount;
-            }
+                double totalAmount = 0.0;
 
-            return totalAmount;
+                for (int i = 0; i < 120; i++)
+                {
+                    totalAmount = totalAmount + totalAmount * 0.07 / 12.0 + inputAmount;
+                }
+
+                return totalAmount;
+            }
+            else throw new System.ArgumentOutOfRangeException("Wrong amount");
         }
 
         public static double EstimateTenYearsAmount_OneTime(double inputAmount)
